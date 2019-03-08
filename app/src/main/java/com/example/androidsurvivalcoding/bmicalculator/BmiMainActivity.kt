bcb27e3b7cb22ff28d38914fd0e0d89cbf618c60
GeoN5 <sgepyh2916@naver.com>
2019-03-08 22:05:02 +0900

    BmiCalculator layout implement

---
+++ app/src/main/java/com/example/androidsurvivalcoding/bmicalculator/BmiMainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.androidsurvivalcoding.bmicalculator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.androidsurvivalcoding.R
+
+class BmiMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/androidsurvivalcoding/bmicalculator/BmiResultActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidsurvivalcoding.bmicalculator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.androidsurvivalcoding.R
+
+class BmiResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi_result)
+    }
+}
