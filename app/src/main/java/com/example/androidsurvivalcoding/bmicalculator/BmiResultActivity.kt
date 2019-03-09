8998246d3d49a561f921947d86af396db40efd75
GeoN5 <sgepyh2916@naver.com>
2019-03-09 20:33:31 +0900

    Migrate to AndroidX

--- app/src/main/java/com/example/androidsurvivalcoding/bmicalculator/BmiResultActivity.kt
+++ app/src/main/java/com/example/androidsurvivalcoding/bmicalculator/BmiResultActivity.kt
@@ -1,7 +1,7 @@
 package com.example.androidsurvivalcoding.bmicalculator
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androidsurvivalcoding.R
 
 class BmiResultActivity : AppCompatActivity() {
