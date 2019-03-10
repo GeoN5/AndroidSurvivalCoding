6fcd0df4b5cb802eac99aea3cee2304179171b37
GeoN5 <sgepyh2916@naver.com>
2019-03-10 20:23:13 +0900

    BmiCalculator make complete

--- app/src/main/java/com/example/androidsurvivalcoding/bmicalculator/BmiMainActivity.kt
+++ app/src/main/java/com/example/androidsurvivalcoding/bmicalculator/BmiMainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.androidsurvivalcoding.bmicalculator
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidsurvivalcoding.R
+import kotlinx.android.synthetic.main.activity_bmi_main.*
+import org.jetbrains.anko.startActivity
 
 class BmiMainActivity : AppCompatActivity() {
 
@@ -10,6 +13,50 @@ class BmiMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi_main)
 
+        loadData()
 
+        resultButton.setOnClickListener {
+            heightEditTextLayout.error = null
+            weightEditTextLayout.error = null
+
+            if(heightEditText.text!!.isNotEmpty() && weightEditText.text!!.isNotEmpty()) {
+                saveData(heightEditText.text.toString().toInt(),weightEditText.text.toString().toInt())
+
+                startActivity<BmiResultActivity>(
+                    "height" to heightEditText.text.toString().toInt(),
+                    "weight" to weightEditText.text.toString().toInt()
+                )
+            }else{
+                if(heightEditText.text!!.isEmpty()){
+                    heightEditTextLayout.error = "값을 입력해주세요."
+                }
+                if(weightEditText.text!!.isEmpty()){
+                    weightEditTextLayout.error = "값을 입력해주세요."
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadData()
+    }
+
+    private fun saveData(height:Int, weight:Int){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT",height).putInt("KEY_WEIGHT",weight).apply()
+    }
+
+    private fun loadData(){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val height = pref.getInt("KEY_HEIGHT",0)
+        val weight = pref.getInt("KEY_WEIGHT",0)
+
+        if(height != 0 && weight != 0){
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
+        }
     }
 }
--- app/src/main/java/com/example/androidsurvivalcoding/bmicalculator/BmiResultActivity.kt
+++ app/src/main/java/com/example/androidsurvivalcoding/bmicalculator/BmiResultActivity.kt
@@ -3,11 +3,49 @@ package com.example.androidsurvivalcoding.bmicalculator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidsurvivalcoding.R
+import kotlinx.android.synthetic.main.activity_bmi_result.*
+import org.jetbrains.anko.longToast
 
 class BmiResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi_result)
+
+        val height = intent.getIntExtra("height",0)
+        val weight = intent.getIntExtra("weight",0)
+        //몸무게 / ( 키 / 100.0의 2.0 제곱값)
+        val bmi = weight / Math.pow(height / 100.0, 2.0)
+
+        longToast("$bmi")
+
+        when{
+            bmi >= 35 -> {
+                resultTextView.text = "고도 비만"
+                imageView.setImageResource(R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
+            }
+            bmi >= 30 -> {
+                resultTextView.text = "2단계 비만"
+                imageView.setImageResource(R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
+            }
+            bmi >= 25 -> {
+                resultTextView.text = "1단계 비만"
+                imageView.setImageResource(R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
+            }
+            bmi >= 23 -> {
+                resultTextView.text = "과체중"
+                imageView.setImageResource(R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
+            }
+            bmi >= 18.5 -> {
+                resultTextView.text = "정상"
+                imageView.setImageResource(R.drawable.ic_sentiment_satisfied_black_24dp)
+            }
+            else -> {
+                resultTextView.text = "저체중"
+                imageView.setImageResource(R.drawable.ic_sentiment_dissatisfied_black_24dp)
+            }
+        }
+
+
     }
 }
