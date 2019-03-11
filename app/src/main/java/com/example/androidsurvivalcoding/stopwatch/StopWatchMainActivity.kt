99b761fb5a0d7703466ed77452f3f1c1ad37dc87
GeoN5 <sgepyh2916@naver.com>
2019-03-11 11:38:20 +0900

    StopWatch layout implement

---
+++ app/src/main/java/com/example/androidsurvivalcoding/stopwatch/StopWatchMainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidsurvivalcoding.stopwatch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidsurvivalcoding.R
+
+class StopWatchMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stop_watch_main)
+    }
+}
