e64952d86d888dacc806faac51fef594e98e088d
GeoN5 <sgepyh2916@naver.com>
2019-03-12 15:09:52 +0900

    WebBrowser layout implement

---
+++ app/src/main/java/com/example/androidsurvivalcoding/webbrowser/WebMainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidsurvivalcoding.webbrowser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidsurvivalcoding.R
+
+class WebMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_main)
+    }
+}
