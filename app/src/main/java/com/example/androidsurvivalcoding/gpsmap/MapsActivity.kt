49f20b9f235572d8417b6874d83f456ffe43fae5
GeoN5 <sgepyh2916@naver.com>
2019-04-03 20:07:08 +0900

    Periodically receive current location information

--- app/src/main/java/com/example/androidsurvivalcoding/gpsmap/MapsActivity.kt
+++ app/src/main/java/com/example/androidsurvivalcoding/gpsmap/MapsActivity.kt
@@ -1,18 +1,37 @@
 package com.example.androidsurvivalcoding.gpsmap
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.androidsurvivalcoding.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val REQUEST_ACCESS_FINE_LOCATION = 1000
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: MyLocationCallBack
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +40,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         //SupportMapFragment를 가져와서 지도가 준비되면 알림을 받습니다.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationInit()
+    }
+
+    //위치 정보를 얻기 위한 각종 초기화
+    //GPS를 사용하여 10초마다 위치 갱신 그 사이에 다른 앱에서 위치를 갱신했다면 5초마다 확인하여 그 값을 활용하여 배터리 절약
+    private fun locationInit(){
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+        locationCallback = MyLocationCallBack()
+        locationRequest = LocationRequest().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            //위치 정보가 없을 때는 업데이트 안함
+            interval = 10000
+            fastestInterval = 5000
+        }
     }
 
     //지도를 사용할 준비가 되면 콜백 메소드 호출
@@ -30,6 +64,89 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //권한 요청
+        permissionCheck(
+            //위치 정보가 필요한 이유 다이얼로그 표시
+            cancle = { showPermissionInfoDialog() },
+            //현재 위치를 주기적으로 요청 (권한이 필요한 부분)
+            ok = { addLocationListener() })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        removeLocationListener()
+    }
+
+    private fun removeLocationListener(){
+        //현재 위치 요청을 삭제
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun permissionCheck(cancle: () -> Unit, ok: () -> Unit){
+        //위치 권한이 있는지검사
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            //권한이 허용되지 않음
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+                //이전에 권한을 한 번 거부한 적이 있는 경우에 실행할 함수
+                cancle()
+            }else{
+                //권한 요청
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_ACCESS_FINE_LOCATION)
+            }
+        }else{
+            //권한을 수락했을 때 실행할 함수
+            ok()
+        }
+    }
+
+    private fun showPermissionInfoDialog(){
+        alert("현재 위치 정보를 얻으려면 위치 권한이 필요합니다","권한이 필요한 이유"){
+            yesButton {
+                //권한 요청
+                ActivityCompat.requestPermissions(this@MapsActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_ACCESS_FINE_LOCATION)
+            }
+            noButton {}
+        }.show()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun addLocationListener(){
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback,null)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_ACCESS_FINE_LOCATION -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //권한 허용됨
+                    addLocationListener()
+                }else{
+                    //권한 거부
+                    toast("권한 거부 됨")
+                }
+                return
+            }
+        }
+    }
+
+    inner class MyLocationCallBack: LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            val location = locationResult?.lastLocation
+            //GPS 꺼짐 및 현재 위치 정보를 얻을 수 없는 경우 Location 객체 null
+            location?.run {
+                val latLng = LatLng(latitude,longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17F))
+
+                Log.d("MapsActivity","위도: $latitude, 경도: $longitude")
+            }
+        }
     }
 
 }
